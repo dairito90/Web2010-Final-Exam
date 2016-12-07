66784acfc2985bf71da3affbc3012c2da2659415
Dairon Rodriguez <drodriguez@fvi.edu>
2016-12-06 19:15:37 -0500

    Wikipedia-app Final-Exam

---
+++ Js/app.js
@@ -0,0 +1,32 @@
+//i do my best in this part i hope you understand me//
+$("#submit").click(search).val{
+  function getArticles(search){
+
+    $.ajax({
+      url:'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch='+search,
+      success: "",
+      datatype: "json",
+      data:""
+  });
+
+  };
+};
+
+
+
+$("#articles").append(
+
+  var article = "";
+      article += `<div class="articles">`;
+      article += `<h3>${title}</h3>`;
+      article +=`<p>${snippet}</p>`;
+
+);
+
+
+$("#submit").html("title.snippet");
+
+
+
+
+  }
